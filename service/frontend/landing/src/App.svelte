1e8fe5b2472668ada072beecc36fac3abc0ed8f5
Dikower <dmitriy1d01@gmail.com>
2020-10-24 20:59:24 +0300

    Init

--- service/frontend/landing/src/App.svelte
+++ service/frontend/landing/src/App.svelte
@@ -1,9 +1,9 @@
 <script>
-	export let name;
+	import Hero from './sections/Hero.svelte';
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<Hero/>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
---
+++ service/frontend/landing/src/sections/Hero.svelte
@@ -0,0 +1,5 @@
+<script>
+    import HeroTitle from './svgs/HeroTitle.svelte';
+</script>
+
+<HeroTitle/>
